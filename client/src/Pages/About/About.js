db2d15e7a5eb0ac312ed17fcc84b9d2a83272fa9
David Wu <davidjwu92@gmail.com>
2020-02-19 10:25:58 -0800

    responsive card images

--- client/src/Pages/About/About.js
+++ client/src/Pages/About/About.js
@@ -15,65 +15,65 @@ const About = () => {
         </div>
         {/* MARKETABLE SKILLS ROW */}
         <div className="row">
-          <div className="col s12 m3">
+          <div className="col s12 m6 l3">
               {/* INNOVATOR */}
-              <div className="card">
+              <div className="card" id="card">
                 <div className="card-image waves-effect waves-block waves-light">
                   <img className="activator" src={developer}/>
                 </div>
                 <div className="card-content">
-                  <span className="card-title activator grey-text text-darken-4 center">Innovative Web Apps.<i className="material-icons right">more_vert</i></span>
+                  <span className="card-title activator grey-text text-darken-4 center" id="cardTitle">Innovative Web Apps.<i className="material-icons right">more_vert</i></span>
                 </div>
                 <div className="card-reveal">
-                  <span className="card-title grey-text text-darken-4">Web App Innovator<i className="material-icons right">close</i></span>
+                  <span className="card-title grey-text text-darken-4" id="cardRevealTitle">Web App Innovator<i className="material-icons right">close</i></span>
                   <p>In addition to being the focus of my career path, Web Development is one of many creative outlets for myself. The projects in my portfolio display some of my original ideas brought to life through web apps.</p>
                   <p>"Are they even good ideas?" I'll let you be the judge of just how interesting they are; check out LAF and Harmonize, two of my brain-children from my days at UCI's coding bootcamp, in my portfolio section.</p>
                 </div>
               </div>
           </div>
-          <div className="col s12 m3">
+          <div className="col s12 m6 l3">
               {/* JAVASCRIPT DEVELOPER */}
-              <div className="card">
+              <div className="card" id="card">
                 <div className="card-image waves-effect waves-block waves-light">
                   <img className="activator" src={gears}/>
                 </div>
                 <div className="card-content">
-                  <span className="card-title activator grey-text text-darken-4 center">Javascript Developer.<i className="material-icons right">more_vert</i></span>
+                  <span className="card-title activator grey-text text-darken-4 center" id="cardTitle">Javascript Developer.<i className="material-icons right">more_vert</i></span>
                 </div>
                 <div className="card-reveal">
-                  <span className="card-title grey-text text-darken-4">JS Developer<i className="material-icons right">close</i></span>
+                  <span className="card-title grey-text text-darken-4" id="cardRevealTitle">JS Developer<i className="material-icons right">close</i></span>
                   <p>I fell in love with coding as soon as I was introduced to Javascript. The skills practiced during my days as a UCI Physics undergrad translated fluidly as I developed mastery of JS functions, methods, and problem-solving tactics.</p>
                   <p>I write JS code with great attention to detail, with transparent, clean organizational structures, and with persistent problem-solving intent.</p>
                 </div>
               </div>
           </div>
-          <div className="col s12 m3">
+          <div className="col s12 m6 l3">
               {/* RESPONSIVE FRONT-END */}
-              <div className="card">
+              <div className="card" id="card">
                 <div className="card-image waves-effect waves-block waves-light">
                   <img className="activator" src={responsive}/>
                 </div>
                 <div className="card-content">
-                  <span className="card-title activator grey-text text-darken-4 center">Responsive Web Design.<i className="material-icons right">more_vert</i></span>
+                  <span className="card-title activator grey-text text-darken-4 center" id="cardTitle">Responsive Web Design.<i className="material-icons right">more_vert</i></span>
                 </div>
                 <div className="card-reveal">
-                  <span className="card-title grey-text text-darken-4">Responsive Web Design<i className="material-icons right">close</i></span>
+                  <span className="card-title grey-text text-darken-4" id="cardRevealTitle">Responsive Web Design<i className="material-icons right">close</i></span>
                   <p>I am cool, confident, and comfortable writing functioning code in the back end. The same is true for coding the front end.</p>
-                  <p>My web apps are responsive, designed to be viewable from any screen.</p>
+                  <p>My web apps are responsive, designed to be viewable from any screen. This site is completely </p>
                 </div>
               </div>
           </div>
-          <div className="col s12 m3">
+          <div className="col s12 m6 l3">
                 {/* PEOPLE-PERSON */}
-              <div className="card">
+              <div className="card" id="card">
                 <div className="card-image waves-effect waves-block waves-light">
                   <img className="activator" src={adaptable}/>
                 </div>
                 <div className="card-content">
-                  <span className="card-title activator grey-text text-darken-4 center">Adaptable Learner.<i className="material-icons right">more_vert</i></span>
+                  <span className="card-title activator grey-text text-darken-4 center" id="cardTitle">Adaptable Learner.<i className="material-icons right">more_vert</i></span>
                 </div>
                 <div className="card-reveal">
-                  <span className="card-title grey-text text-darken-4">Adaptable Learner<i className="material-icons right">close</i></span>
+                  <span className="card-title grey-text text-darken-4" id="cardRevealTitle">Adaptable Learner<i className="material-icons right">close</i></span>
                   <p>TEXT HERE    </p>
                   <p>TEXT HERE    </p>
                 </div>
