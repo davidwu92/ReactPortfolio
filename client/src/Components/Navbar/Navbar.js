dc69de503025e7d86d6f8063aaca1ee07f92cb40
David Wu <davidjwu92@gmail.com>
2020-02-19 13:02:30 -0800

    Carousel work

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import './NavbarStyle.css'
 import Scrollspy from 'react-scrollspy'
 
--- client/src/Pages/Portfolio/Portfolio.js
+++ client/src/Pages/Portfolio/Portfolio.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Navbar from '../../Components/Navbar'
+import M from "materialize-css";
+import './portfolio.css'
+import adaptable from '../About/adaptable.jpg'
 
-const Portfolio = () => {
+const Portfolio =()=>{
+
+  useEffect(()=>{
+    // Carousel Options
+    let elem = document.querySelector('.carousel');
+    let instance = M.Carousel.init(elem, 
+      { indicators: true, padding: 100 }
+    );
+  }, [])
+  
   return(
     <>
       <Navbar/>
@@ -9,16 +21,22 @@ const Portfolio = () => {
         <div id="pageTitle" className = "row center">
           <h1>Portfolio</h1>
         </div>
-        <div>
-          <p>
-             text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. 
-             text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-             text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
+        {/* CAROUSEL */}
+        <div class="carousel">
+          <a class="carousel-item" href="#one!">
+            <img src={adaptable}/>
+          </a>
+          <a class="carousel-item" href="#two!">
+            <img src={adaptable}/>
+          </a>
+          <a class="carousel-item" href="#three!">
+            <img src={adaptable}/>
+          </a>
+          <a class="carousel-item" href="#four!"><img src={adaptable}/></a>
+          <a class="carousel-item" href="#five!"><img src={adaptable}/></a>
         </div>
-      </div>
+      </div> {/* container end */}
     </>
   )
 }
-
 export default Portfolio
