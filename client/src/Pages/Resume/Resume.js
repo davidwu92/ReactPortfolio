0d60bafe4c8ef62586a8960901a426b232d92a75
David Wu <davidjwu92@gmail.com>
2020-02-24 12:16:53 -0800

    Removed PDF import

--- client/src/Pages/Resume/Resume.js
+++ client/src/Pages/Resume/Resume.js
@@ -1,37 +1,23 @@
 import React, {Component} from 'react'
 import Navbar from '../../Components/Navbar'
-import {Document, Page} from 'react-pdf'
 
-class Resume extends Component {
-  state = {
-    numPages: null,
-    pageNumber: 1,
-  }
-  
-  onDocumentLoadSucccess = ({numPages}) =>{
-    this.setState({numPages});
-  }
-
-  render() {
-    const {pageNumber, numPages} = this.state
-
-    return(
-      <>
-        <Navbar/>
+const Resume = () => {
+  return(
+    <>
+      <Navbar/>
+      <div className="container" id="resumeContainer">
         <div id="pageTitle" className = "row center">
           <h1>Resume</h1>
         </div>
-        <Document
-          file="../../../public/Resume2020.pdf"
-          onLoadSuccess={this.onDocumentLoadSuccess}
-        >
-          {/* <Page pageNumber={pageNumber} /> */}
-        </Document>
-        <p>Page {pageNumber} of {numPages}</p>
-      </>
-    )
-
-  }
+        <div>
+          <p>
+            RESUME HERE. RESUME HERE. RESUME HERE. RESUME HERE. RESUME HERE. RESUME HERE. RESUME HERE. RESUME HERE. RESUME HERE. RESUME HERE. RESUME HERE. RESUME HERE. RESUME HERE. RESUME HERE. RESUME HERE. RESUME HERE. RESUME HERE. RESUME HERE. RESUME HERE. RESUME HERE. RESUME HERE. RESUME HERE. RESUME HERE. RESUME HERE. RESUME HERE. RESUME HERE. 
+          </p>
+        </div>
+      </div>
+    </>
+  )
 }
+
 
 export default Resume
