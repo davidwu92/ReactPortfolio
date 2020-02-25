64a6e3948ea9ffbcfb43c73ae05693832acd995c
David Wu <davidjwu92@gmail.com>
2020-02-25 13:43:55 -0800

    touch drag works on stickers

--- client/src/Components/StickerBoard/StickerBoard.js
+++ client/src/Components/StickerBoard/StickerBoard.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react"
 import StickerAPI from '../../utils/StickerAPI'
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import './stickerBoard.css'
 
 const {getStickers, postSticker} = StickerAPI
 
@@ -33,6 +34,28 @@ const StickerBoard = () => {
       .catch(e=>console.error(e))
     }, [])
 
+  //SAVE AND POST THE DRAGGED STICKER
+  const saveSticker = (xPercent, yPercent) => {
+    if (xPercent > 0.05 && xPercent < 0.91 && yPercent > 0.05 && yPercent <0.91){
+      sessionStorage.getItem("count") ? sessionStorage.setItem("count", parseInt(sessionStorage.getItem('count')) +1) :
+      sessionStorage.setItem("count", 1)
+      postSticker({animal: stickerState.id, positionX: xPercent, positionY: yPercent})
+          .then(()=>{
+            getStickers()
+            .then(({data}) => {
+              let stickerArray = []
+              data.forEach(sticker => {
+                let arr = []
+                arr.push(sticker.animal, sticker.positionX, sticker.positionY)
+                stickerArray.push(arr)
+              })
+              setArrayState({stickers: stickerArray})
+            })
+            .catch(e=>console.error(e))
+          })
+          .catch(e=>console.error(e))
+    }
+  }
 //Clicked on and started dragging a sticker...
   const dragStart = e => {
     const sticker_id = e.target.id
@@ -42,43 +65,41 @@ const StickerBoard = () => {
   const dragOver = e => {
     e.stopPropagation()
   }
-
   toast.configure();
   const toastOptions = {
     autoClose: 7000,
     hideProgressBar: true,
     type: "error"
   }
-
 //END dragged sticker.
   const dragEnd = e =>{
-    // console.log(e.view)
-    sessionStorage.getItem("count") ? sessionStorage.setItem("count", parseInt(sessionStorage.getItem('count')) +1) :
-    sessionStorage.setItem("count", 1)
-
     if (sessionStorage.getItem("count") <= 20) {
-      postSticker({animal: stickerState.id, positionX: e.pageX/window.innerWidth, positionY: e.pageY/window.innerHeight})
-      .then(()=>{
-        getStickers()
-        .then(({data}) => {
-          let stickerArray = []
-          data.forEach(sticker => {
-            let arr = []
-            arr.push(sticker.animal, sticker.positionX, sticker.positionY)
-            stickerArray.push(arr)
-          })
-          setArrayState({stickers: stickerArray})
-        })
-        .catch(e=>console.error(e))
-      })
-      .catch(e=>console.error(e))
+      let xPercent = e.pageX/window.innerWidth
+      let yPercent = e.pageY/window.innerHeight
+      saveSticker(xPercent, yPercent)
     } else {
-      toast("You've reached the sticker limit (20). Please leave some room for others to decorate!", toastOptions)
+      toast("You've reached the sticker limit (20). I hope you enjoyed", toastOptions)
     }
-    //LOCAL ONLY
-    // let dropInfo = arrayState.stickers
-    // dropInfo.push([stickerState.id, e.pageX, e.pageY])
-    // setArrayState({stickers: dropInfo})
+  }
+
+//TOUCH-DRAG functions
+  const touchStart = e =>{
+    // console.log(e.currentTarget.id)
+    const sticker_id = e.currentTarget.id
+    setStickerState({...stickerState, id:sticker_id})
+  }
+  // const touchOver = e => {
+  //   e.preventDefault()
+  //   e.stopPropagation()
+  // }
+  const touchEnd = e =>{
+    if (sessionStorage.getItem("count") <= 20) {
+      let xPercent = e.nativeEvent.changedTouches[0].pageX/window.innerWidth
+      let yPercent = e.nativeEvent.changedTouches[0].pageY/window.innerHeight
+      saveSticker(xPercent, yPercent)
+    } else {
+      toast("You've reached the sticker limit (20). I hope you enjoyed", toastOptions)
+    }
   }
 
   const toggleStickers = () => {
@@ -97,32 +118,20 @@ const StickerBoard = () => {
       <div id="stickerContainer">
         {/* <Stickers/> */}
             <div className="row" id="stickerBar"> 
+
                 <div
-                    id="dove"
-                    draggable="true"
-                    onDragStart={dragStart}
-                    onDragOver={dragOver}
-                    onDragEnd={dragEnd}
-                >
-                  <i className="fas fa-dove"></i>
-                </div>
-                <div
-                    id="crow"
-                    draggable="true"
-                    onDragStart={dragStart}
-                    onDragOver={dragOver}
-                    onDragEnd={dragEnd}
-                >
-                  <i className="fas fa-crow"></i>
-                </div>
-                <div
-                    id="cat"
-                    draggable="true"
-                    onDragStart={dragStart}
-                    onDragOver={dragOver}
-                    onDragEnd={dragEnd}
-                >
-                  <i className="fas fa-cat"></i>
+                  id="cat"
+                  draggable="true"
+                  onDragStart={dragStart}
+                  onDragOver={dragOver}
+                  onDragEnd={dragEnd}
+                  onTouchStart={touchStart}
+                  // onTouchMove={touchOver} PROBABLY don't need this.
+                  onTouchEnd={touchEnd}
+                  >
+                    <button className="btn-small btn-floating white">
+                      <i id="barIcon" className="fas fa-cat"></i>
+                    </button>
                 </div>
                 <div
                   id="dog"
@@ -130,8 +139,12 @@ const StickerBoard = () => {
                   onDragStart={dragStart}
                   onDragOver={dragOver}
                   onDragEnd={dragEnd}
-                >
-                  <i className="fas fa-dog"></i>
+                  onTouchStart={touchStart}
+                  onTouchEnd={touchEnd}
+                  >
+                    <button className="btn-small btn-floating white">
+                      <i id="barIcon" className="fas fa-dog"></i>
+                    </button>
                 </div>
                 <div
                   id="fish"
@@ -139,17 +152,51 @@ const StickerBoard = () => {
                   onDragStart={dragStart}
                   onDragOver={dragOver}
                   onDragEnd={dragEnd}
-                >
-                  <i className="fas fa-fish"></i>
+                  onTouchStart={touchStart}
+                  onTouchEnd={touchEnd}
+                  >
+                    <button className="btn-small btn-floating white">
+                      <i id="barIcon" className="fas fa-fish"></i>
+                    </button>
                 </div>
                 <div
-                    id="dragon"
-                    draggable="true"
-                    onDragStart={dragStart}
-                    onDragOver={dragOver}
-                    onDragEnd={dragEnd}
-                >
-                  <i className="fas fa-dragon"></i>
+                  id="frog"
+                  draggable="true"
+                  onDragStart={dragStart}
+                  onDragOver={dragOver}
+                  onDragEnd={dragEnd}
+                  onTouchStart={touchStart}
+                  onTouchEnd={touchEnd}
+                  >
+                    <button className="btn-small btn-floating white">
+                      <i id="barIcon" className="fas fa-frog"></i>
+                    </button>
+                </div>
+                <div
+                  id="crow"
+                  draggable="true"
+                  onDragStart={dragStart}
+                  onDragOver={dragOver}
+                  onDragEnd={dragEnd}
+                  onTouchStart={touchStart}
+                  onTouchEnd={touchEnd}
+                  >
+                    <button className="btn-small btn-floating white">
+                      <i id="barIcon" className="fas fa-crow"></i>
+                    </button>
+                </div>
+                <div
+                  id="dragon"
+                  draggable="true"
+                  onDragStart={dragStart}
+                  onDragOver={dragOver}
+                  onDragEnd={dragEnd}
+                  onTouchStart={touchStart}
+                  onTouchEnd={touchEnd}
+                  >
+                    <button className="btn-small btn-floating white">
+                      <i id="barIcon" className="fas fa-dragon"></i>
+                    </button>
                 </div>
                 <button className="btn-small btn-floating black" onClick={toggleStickers}>
                   <i className="fas fa-eye"></i>
